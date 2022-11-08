dfb86b71911e59ad3ba1672ea0a31970801a1a9b
박상천 <pswlove38@naver.com>
2022-11-08 17:14:41 +0900

    stack 구현

---
+++ Stack/스택(10828).py
@@ -0,0 +1,26 @@
+from sys import stdin
+N = int(stdin.readline())
+
+stack = []
+
+for i in range(N):
+    command = stdin.readline().split()
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[len(stack)-1])
