ca99fc9062067f4a9e0f91efb0339728fd43c1e6
박상천 <pswlove38@naver.com>
2022-11-13 01:17:13 +0900

    bfs 기초 문제

--- 스타트와링크(14889).py
+++ 스타트와링크(14889).py
@@ -1,9 +1,24 @@
 from sys import stdin
+from itertools import permutations
 
-N = stdin.readline()
+N = int(stdin.readline())
 status = []
+str = [1, 2, 3]
+set = list(permutations(str, 2))
 
 for i in range(N):
-    status.append(list(map(int, stdin.readline().split(''))))
+    status.append(list(map(int, stdin.readline().split())))
 
-print(status)
+def cul(str, status): # 맴버 리스트를 받아 모든 조합의 합을 구하는 함수
+    set = list(permutations(str, 2))
+    result = 0
+    for i in set:
+        result += status[i[0]-1][i[1]-1]
+    return result
+
+print(cul(str, status))
+print(set)
+print(status[0])
+print(status[1])
+print(status[2])
+print(status[3])
