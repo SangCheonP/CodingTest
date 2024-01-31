f2c9d90c8490a5e76d2edd3d0b0cb188642826c1
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2024-01-31 22:26:50 +0900

    DP, 달팽이 숫자 완료

--- src/DP/Baek_11660.java
+++ src/DP/Baek_11660.java
@@ -35,31 +35,53 @@ public class Baek_11660 {
             }
         }
 
+        // 맨 위 행
+        for(int j = 1; j < N; j++){
+            map[0][j] = map[0][j] + map[0][j - 1];
+        }
 
+        // 맨 왼쪽 열
+        for(int i = 1; i < N; i++){
+            map[i][0] = map[i][0] + map[i - 1][0];
+        }
 
+        for(int i = 1; i < N; i++){
+            for(int j = 1; j < N; j++){
+                //          자신          위               왼쪽              겹치는 것
+                map[i][j] = map[i][j] + map[i - 1][j] + map[i][j - 1] - map[i - 1][j - 1];
+            }
+        }
+
+//        for(int i = 0; i < N; i++){
+//            for(int j = 0; j < N; j++){
+//                System.out.print(map[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
 
         // M개의 줄동안 좌표를 받음
         for(int z = 0; z < M; z++){
             st = new StringTokenizer(br.readLine());
-            int i1 = Integer.parseInt(st.nextToken());
-            int j1 = Integer.parseInt(st.nextToken());
-            int i2 = Integer.parseInt(st.nextToken());
-            int j2 = Integer.parseInt(st.nextToken());
+            int i1 = Integer.parseInt(st.nextToken()) - 1;
+            int j1 = Integer.parseInt(st.nextToken()) - 1;
+            int i2 = Integer.parseInt(st.nextToken()) - 1;
+            int j2 = Integer.parseInt(st.nextToken()) - 1;
 
             int result = 0;
 
-            i1--;
-            j1--;
-            i2--;
-            j2--;
-
-            // 줄마다
-            for(int x = i1; x <= i2; x++){
-                if(j1 - 1 < 0)
-                    result += (map[x][j2]);
-                else
-                    result += (map[x][j2] - map[x][j1 - 1]);
+            // 0, 0 부터 
+            if(i1 == 0 && j1 == 0){
+                result += map[i2][j2];
+                
+            }else if(i1 == 0){ // 위쪽이 없음
+                result = map[i2][j2] - map[i2][j1 - 1];
+                
+            } else if (j1 == 0) { // 왼쪽이 없음
+                result = map[i2][j2] - map[i1 - 1][j2];
+                
+            }else{ // 둘 다 있음
+                result = map[i2][j2] - map[i2][j1 -1] - map[i1 - 1][j2] + map[i1 - 1][j1 - 1];
             }
 
             System.out.println(result);
