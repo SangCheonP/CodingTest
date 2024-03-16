04793956dcb2d8b058d87b1b7a8f8232c3ca13e3
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2024-03-16 22:42:22 +0900

    Practice Update

---
+++ src/DP/baek_11726_2xn타일링.java
@@ -0,0 +1,10 @@
+package DP;
+/*
+백준 11726 2xn 타일링(실버3)
+https://www.acmicpc.net/problem/11726
+ */
+public class baek_11726_2xn타일링 {
+    public static void main(String[] args) {
+        
+    }
+}
