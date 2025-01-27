10f210f6de6205d43400485983a541dd519e7a38
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2025-01-27 15:57:07 +0900

    Create Programmers_올바른 괄호.java

---
+++ src/Queue/Programmers_올바른 괄호.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String s) {
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                // 여는 괄호를 큐에 삽입
+                queue.offer(c);
+            } else {
+                // 닫는 괄호 처리
+                if (queue.isEmpty()) {
+                    // 닫는 괄호가 큐에 남아 있는 여는 괄호보다 많으면 false
+                    return false;
+                }
+                queue.poll(); // 큐에서 가장 먼저 들어간 여는 괄호 제거
+            }
+        }
+
+        // 큐가 비어 있으면 올바른 괄호, 남아 있으면 잘못된 괄호
+        return queue.isEmpty();
+    }
+}
