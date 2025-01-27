fa496780c6c82ade5f3f67fe5a5343c3f9e0bdc9
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2025-01-27 10:37:59 +0900

    Create Programmers_전화번호 목록.java

---
+++ src/Map/Programmers_전화번호 목록.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        // 1. Map 생성 및 모든 전화번호 저장
+        Map<String, Integer> map = new HashMap<>();
+        for(String phone : phone_book){
+            map.put(phone, null);
+        }
+        
+        // 2. 각 수를 돌아가며 접두사가 있는지 체크
+        for(String phone : phone_book){
+            for(int i = 1; i < phone.length(); i++){
+                if(map.containsKey(phone.substring(0, i))){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+        
+    }
+}
