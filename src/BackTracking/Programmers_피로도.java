a15d28c5d0b6bf899609686fe5e7cda3027f0e86
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2025-01-31 19:00:16 +0900

    Create Programmers_피로도.java

---
+++ src/BackTracking/Programmers_피로도.java
@@ -0,0 +1,26 @@
+class Solution {
+    static int result = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        
+        explore(k, dungeons, visited, 0);
+        
+        return result;
+    }
+    
+    // [최소 피로도, 소모 피로도] >=
+    static void explore(int k, int[][] dungeons, boolean[] visited, int cur){
+        for(int i = 0; i < dungeons.length; i++){
+            if(visited[i]) continue; // 방문했으면 패스
+            
+            if(k >= dungeons[i][0]){
+                visited[i] = true;
+                result = Math.max(result, cur + 1); // 방문 횟수 갱신
+                explore(k - dungeons[i][1], dungeons, visited, cur + 1); // 방문
+                visited[i] = false;
+            }
+            
+        }
+    }
+}
