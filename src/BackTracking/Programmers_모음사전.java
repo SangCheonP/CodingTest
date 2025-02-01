b65c0ba836c4a26e6c161f5879412558aa233e5a
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2025-02-01 21:45:19 +0900

    Create Programmers_모음사전.java

---
+++ src/BackTracking/Programmers_모음사전.java
@@ -0,0 +1,25 @@
+/**
+*  1. 완전 탐색 재귀로 모든 경우의 수 체크
+*  2. 리스트에 저장하고, 해당 단어의 위치를 반환
+**/
+
+import java.util.*;
+
+class Solution {
+    static String[] vowels = {"A", "E", "I", "O", "U"};
+    static List<String> list = new ArrayList<>();
+    
+    public int solution(String word) {
+        generateWords("", 0);
+        return list.indexOf(word);
+    }
+    
+    void generateWords(String cur, int dep){
+        if(dep > 5) return; // 최대 5글자
+        list.add(cur);
+        
+        for(String vowel : vowels){
+            generateWords(cur + vowel, dep + 1);
+        }
+    }
+}
