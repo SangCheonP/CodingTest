f0b9c871d94404ea78816de3852f80a64f717900
SangCheonP <pswlove38@naver.com>
2026-01-20 18:56:13 +0900

    .

--- src/DP/Baek_1463_1로만들기.java
+++ src/DP/Baek_1463_1로만들기.java
@@ -1,29 +1,27 @@
 package DP;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-/*
-백준 1463 1로 만들기(실버3)
-https://www.acmicpc.net/problem/1463
- */
 public class Baek_1463_1로만들기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N+1];
 
-        for (int i = 2; i < N+1; i++) {
-            dp[i] = dp[i-1] + 1;
-            if(i % 3 == 0){
-                dp[i] = Math.min(dp[i/3] + 1, dp[i]);
-            }
+        int[] map = new int[N + 1];
+        map[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            map[i] = map[i - 1] + 1;
             if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i/2] + 1, dp[i]);
+                map[i] = Math.min(map[i / 2] + 1, map[i]);
+            }
+            if (i % 3 == 0) {
+                map[i] = Math.min(map[i / 3] + 1, map[i]);
             }
         }
-        System.out.println(dp[N]);
+
+        System.out.println(map[N]);
     }
 }
