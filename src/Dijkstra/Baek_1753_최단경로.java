2c27eaf089e542b8342493cb70001002c433f282
SangCheonP <pswlove38@naver.com>
2026-01-24 12:56:58 +0900

    .

--- src/Dijkstra/Baek_1753_최단경로.java
+++ src/Dijkstra/Baek_1753_최단경로.java
@@ -1,87 +1,72 @@
 package Dijkstra;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Baek_1753_최단경로 {
-    static class Node{
-        int to, w;
-        Node next;
-
-        Node(int to, int w, Node next){
-            this.to = to;
-            this.w = w;
-            this.next = next;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int INF = Integer.MAX_VALUE;
-
-        // 정점: 1 ~ V
-        // V: 정점의 개수 / E: 간선의 개수
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
 
-        // 인접리스트
-        Node[] adjList = new Node[V+1];
-        // 해당 정점까지의 최단 거리
-        int[] minDis = new int[V+1];
-        boolean[] visited = new boolean[V+1];
+        int start = Integer.parseInt(br.readLine());
 
-        int str = Integer.parseInt(br.readLine());
-
-        Arrays.fill(minDis, INF);
-
-        // 시작 정점 초기화
-        minDis[str] = 0;
+        List<List<Node>> graph = new ArrayList<>();
+        for (int i = 0; i <= V; i++) {
+            graph.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            adjList[from] = new Node(to, w, adjList[from]);
+            graph.get(u).add(new Node(v, w));
         }
 
-        int cnt;
-        for (cnt = 0; cnt < V; cnt++) {
-            int min = Integer.MAX_VALUE;
-            int minVertex = -1;
+        int[] distance = new int[V + 1];
+        int INF = Integer.MAX_VALUE;
+        Arrays.fill(distance, INF);
 
-            // 방문X + 가중치가 가장 작은 정점 선택
-            for (int i = 1; i < V+1; i++) {
-                if(!visited[i] && minDis[i] < min){
-                    min = minDis[i];
-                    minVertex = i;
-                }
-            }
+        distance[start] = 0;
 
-            // 없으면
-            if(minVertex == -1) break;
-            visited[minVertex] = true;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
 
-            // 해당 정점을 경유지로 하는 다른 정점의 가중치들을 업데이트
-            for (Node tmp = adjList[minVertex]; tmp != null; tmp = tmp.next) {
-                if(!visited[tmp.to] && minDis[tmp.to] > + min + tmp.w){
-                    minDis[tmp.to] = min + tmp.w;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            if (cur.w > distance[cur.n]) continue;
+
+            for (Node next : graph.get(cur.n)) {
+                if (distance[next.n] > cur.w + next.w) {
+                    distance[next.n] = cur.w + next.w;
+                    pq.offer(new Node(next.n, cur.w + next.w));
                 }
             }
         }
 
-        for (int i = 1; i < V+1; i++) {
-            if(minDis[i] == INF){
-                System.out.println("INF");
-            }else{
-                System.out.println(minDis[i]);
-            }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            sb.append(distance[i] == INF ? "INF" : distance[i]).append("\n");
         }
+        System.out.println(sb);
+    }
+}
+
+class Node implements Comparable<Node>{
+    int n, w;
+
+    public Node (int n, int w) {
+        this.n = n;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.w - o.w;
     }
 }
