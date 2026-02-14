3a69cfe6e595de797bd3f1f5a64ed3cb56c2417f
SangCheonP <pswlove38@naver.com>
2026-02-14 22:38:47 +0900

    .

--- src/Comb/Baek_15686_치킨배달.java
+++ src/Comb/Baek_15686_치킨배달.java
@@ -1,78 +1,68 @@
 package Comb;
 
 import java.io.*;
-        import java.util.*;
-
-/**
- * 시간 : 도시의 개수 * 치킨집 총 개수 C m = 100 * 13 C m -> 1억 미만이라 충붖ㄴ
- * 공간 : 리스트 2개 및 변수만 사용해서 충분
- * 예외 :
- */
+import java.util.*;
 
 public class Baek_15686_치킨배달 {
-    static List<Point> homes, chickens;
-    static int result;
+    static List<Point> homes, chks;
+    static int N, M, result;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         homes = new ArrayList<>();
-        chickens = new ArrayList<>();
+        chks = new ArrayList<>();
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
+
             for (int j = 1; j <= N; j++) {
-                int minCkLen = Integer.parseInt(st.nextToken());
-                if (minCkLen == 1) {
+                int cur = Integer.parseInt(st.nextToken());
+                if (cur == 1) {
                     homes.add(new Point(i, j));
-                } else if (minCkLen == 2){
-                    chickens.add(new Point(i, j));
+                } else if (cur == 2) {
+                    chks.add(new Point(i, j));
                 }
             }
         }
 
         result = Integer.MAX_VALUE;
-
-        comb(M, 0, new ArrayList<>());
+        comb(new ArrayList<>(), 0);
 
         System.out.println(result);
     }
 
-    public static void comb(int M, int start, List<Integer> cur) {
-        if(cur.size() == M) {
-            int cityCkLen = 0;
+    static void comb(List<Point> sel, int start) {
+        if (sel.size() == M) {
+            int allChkLen = 0;
             for (Point home : homes) {
-                int minCkLen = Integer.MAX_VALUE;
-                for (int ckIdx : cur) {
-                    Point ck = chickens.get(ckIdx);
-                    int len = Math.abs(home.i - ck.i) + Math.abs(home.j - ck.j);
-                    minCkLen = Math.min(minCkLen, len);
+                int temp = Integer.MAX_VALUE;
+                for (Point chk : sel) {
+                    temp = Math.min(temp, Math.abs(home.i - chk.i) + Math.abs(home.j - chk.j));
                 }
-
-                cityCkLen += minCkLen;
+                allChkLen += temp;
             }
 
-            result = Math.min(result, cityCkLen);
+            result = Math.min(result, allChkLen);
             return;
         }
 
-        for (int i = start; i < chickens.size(); i++) {
-            cur.add(i);
-            comb(M, i + 1, cur);
-            cur.remove(cur.size() - 1);
+        for (int i = start; i < chks.size(); i++) {
+            sel.add(chks.get(i));
+            comb(sel, i + 1);
+            sel.remove(sel.size() - 1);
         }
     }
 }
 
-
 class Point {
     int i, j;
 
-    public Point(int i, int j) {
+    public Point(int i , int j) {
         this.i = i;
         this.j = j;
     }
