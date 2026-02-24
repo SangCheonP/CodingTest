6e92dae4f9ed91331857e808cf9b83c3a6c6968a
SangCheonP <pswlove38@naver.com>
2026-02-24 19:19:30 +0900

    .

--- src/DFS/Baek_1987_알파벳.java
+++ src/DFS/Baek_1987_알파벳.java
@@ -4,21 +4,17 @@ package DFS;
 https://www.acmicpc.net/problem/1987
  */
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Baek_1987_알파벳 {
-    public static int R, C, result;
-    public static char[][] map;
-    public static int[] di = {-1, 1, 0, 0};
-    public static int[] dj = {0, 0, -1, 1};
+    static int result = 0, R, C;
+    static int[] di = {1, -1, 0, 0};
+    static int[] dj = {0, 0, 1, -1};
+    static char[][] map;
+    static boolean[] visited = new boolean[26];
 
-    public static Set<Character> set;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -26,40 +22,34 @@ public class Baek_1987_알파벳 {
         C = Integer.parseInt(st.nextToken());
 
         map = new char[R][C];
-
-        // 지나온 알파벳 저장 집합
-        set = new HashSet<>();
-
         for (int i = 0; i < R; i++) {
-            map[i] = br.readLine().toCharArray();
+            String temp = br.readLine();
+            for (int j = 0; j < C; j++) {
+                map[i][j] = temp.charAt(j);
+            }
         }
 
-        // 시작점 추가
-        set.add(map[0][0]);
-        result = 0;
         dfs(0, 0, 1);
+
         System.out.println(result);
     }
 
-    public static void dfs(int i, int j, int cnt){
-        result = Math.max(result, cnt);
-        // 4방향
+    static void dfs(int i, int j, int count) {
+        visited[map[i][j] - 'A'] = true;
+        result = Math.max(result, count);
+
         for (int d = 0; d < 4; d++) {
             int ni = i + di[d];
             int nj = j + dj[d];
 
-            // 범위 안
-            if (0 <= ni && ni < R && 0 <= nj && nj < C){
-                // 새로운 알파벳이면
-                if(!set.contains(map[ni][nj])){
-                    // 집합에 넣고 dfs
-                    set.add(map[ni][nj]);
-                    dfs(ni, nj, cnt+1);
+            if (0 <= ni && ni < R && 0 <= nj && nj < C) {
+                int nextIdx = map[ni][nj] - 'A';
+                if (!visited[nextIdx]) {
+                    dfs(ni, nj, count + 1);
                 }
             }
         }
-        // 끝부분 도달시
-        set.remove(map[i][j]);
 
+        visited[map[i][j] - 'A'] = false;
     }
 }
