57382c5cae781d7ad6c54d4be6c6cd9e1fe4aae4
SangCheonP <pswlove38@naver.com>
2026-03-03 22:56:14 +0900

    .

--- src/BinarySearch/Beak_2110_공유기설치.java
+++ src/BinarySearch/Beak_2110_공유기설치.java
@@ -3,12 +3,6 @@ package BinarySearch;
 import java.util.*;
 import java.io.*;
 
-/**
- * 시간 : sort + N * logN(집 배열 돌기 * 이분 탐색) = 200,000 * 30 = 6,000,000
- * 공간 : N 크기 Long 배열 한 개
- * 예외 : long 처리
- */
-
 public class Beak_2110_공유기설치 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,38 +11,36 @@ public class Beak_2110_공유기설치 {
         int N = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
 
-        Long[] homes = new Long[N];
+        long[] homes = new long[N];
         for (int i = 0; i < N; i++) {
             homes[i] = Long.parseLong(br.readLine());
         }
 
         Arrays.sort(homes);
 
-        long low = 1, high = homes[N - 1];
-        long result = Long.MIN_VALUE;
-        while (low <= high) {
-            long mid = low + (high - low) / 2;
+        long left = 1, right = homes[N - 1] - homes[0], result = 0;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
 
-            if (install(mid, C, homes)) {
-                result = Math.max(result, mid);
-                low = mid + 1;
+            if (install(homes, mid, C)) {
+                result = mid;
+                left = mid + 1;
             } else {
-                high = mid - 1;
+                right = mid - 1;
             }
         }
 
         System.out.println(result);
     }
 
-    static boolean install(long mid, int target, Long[] homes) {
-        long cur = homes[0];
-        int cnt = 1;
+    static boolean install(long[] homes, long mid, int C) {
+        int cnt = 1, prevInstall = 0;
 
         for (int i = 1; i < homes.length; i++) {
-            if (homes[i] - cur >= mid) {
-                cur = homes[i];
+            if (homes[i] - homes[prevInstall] >= mid) {
+                prevInstall = i;
                 cnt++;
-                if (target == cnt) return true;
+                if (cnt >= C) return true;
             }
         }
 
